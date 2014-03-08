7fb7b743f40147ed4ada55b9b665f713631872b7
Jon Sagara <jonsagara@gmail.com>
2014-03-08 13:12:10 -0800

    Initial commit.

---
+++ src/AddressForm.MvcWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AddressForm.MvcWeb.Startup))]
+namespace AddressForm.MvcWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
