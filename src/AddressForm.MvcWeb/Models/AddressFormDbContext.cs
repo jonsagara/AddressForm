b59025022e839e4b0d83459b5e57677132211e6e
Jon Sagara <jonsagara@gmail.com>
2014-03-08 13:22:08 -0800

    Moved the DbContext into its own file. Renamed IdentityModels.cs to ApplicationUser.cs.

--- src/AddressForm.MvcWeb/Models/AddressFormDbContext.cs
+++ src/AddressForm.MvcWeb/Models/AddressFormDbContext.cs
@@ -2,11 +2,6 @@
 
 namespace AddressForm.MvcWeb.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-    }
-
     public class AddressFormDbContext : IdentityDbContext<ApplicationUser>
     {
         public AddressFormDbContext()
