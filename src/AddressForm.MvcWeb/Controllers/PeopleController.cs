a8cc1b96ed49c1731d72c157def8c60dc5d8be7d
Jon Sagara <jonsagara@gmail.com>
2014-03-10 11:07:22 -0700

    Removed DictionaryKeysAreNotPropertyNamesJsonConverter, as I wasn't using it.

--- src/AddressForm.MvcWeb/Controllers/PeopleController.cs
+++ src/AddressForm.MvcWeb/Controllers/PeopleController.cs
@@ -1,55 +1,14 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using AddressForm.MvcWeb.Models;
 using AutoMapper;
-using Newtonsoft.Json;
 
 namespace AddressForm.MvcWeb.Controllers
 {
-    /// <summary>
-    /// JSON converter for IDictionary that ignores the contract resolver (e.g. CamelCasePropertyNamesContractResolver)
-    /// when converting dictionary keys to property names.
-    /// </summary>
-    public class DictionaryKeysAreNotPropertyNamesJsonConverter : JsonConverter
-    {
-        public override bool CanConvert(Type objectType)
-        {
-            return typeof(IDictionary).IsAssignableFrom(objectType);
-        }
-
-        public override bool CanRead
-        {
-            get { return false; }
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            throw new InvalidOperationException();
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            IDictionary dictionary = (IDictionary)value;
-
-            writer.WriteStartObject();
-
-            foreach (DictionaryEntry entry in dictionary)
-            {
-                string key = Convert.ToString(entry.Key, CultureInfo.InvariantCulture);
-                writer.WritePropertyName(key);
-                serializer.Serialize(writer, entry.Value);
-            }
-
-            writer.WriteEndObject();
-        }
-    }
-
     public class PeopleController : AddressFormBaseController
     {
         public async Task<ActionResult> New()
